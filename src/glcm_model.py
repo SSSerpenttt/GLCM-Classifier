b1e7df6383a8ef209d207de518b78f7cf2093072
SSSerpenttt <77667524+SSSerpenttt@users.noreply.github.com>
2025-07-02 22:28:39 +0800

    Update glcm_model.py

--- src/glcm_model.py
+++ src/glcm_model.py
@@ -189,7 +189,7 @@ class GLCMModel:
         features = []
         valid_roi_indices = []
         for task_batch in batch(tasks, n=1000):  # Adjust batch size if needed
-            results = Parallel(n_jobs=128, backend='loky')(
+            results = Parallel(n_jobs=128, backend='threading')(
                 delayed(process_roi)(img, roi, i, j)
                 for img, roi, i, j in task_batch
             )
